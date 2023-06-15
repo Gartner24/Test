18eef9ae9b36647186e847815d1983c9c7674e66
Santiago <santiagovalencialeon@gmail.com>
2023-06-15 09:55:00 -0500

    changes in css

---
+++ main.js
@@ -0,0 +1,8 @@
+const buttonTest = document.getElementById("start");
+
+// test the button for when clicking send an alert
+
+buttonTest.addEventListener("click", () => {
+    alert("Hello World");
+}
+)
